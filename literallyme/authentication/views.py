25754a92901bae97b86444b83764deb7200fda9d
Kragline <armanaraqelyan1107@gmail.com>
2023-08-11 08:15:11 -0700

    optimized authentication viewes.py

--- literallyme/authentication/views.py
+++ literallyme/authentication/views.py
@@ -4,19 +4,12 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout, login
 from django.views.generic import CreateView
 
-from mainapp.utils import DataMixin
 from .forms import *
 
 
-class RegisterUserView(DataMixin, CreateView):
+class RegisterUserView(CreateView):
     form_class = RegisterUserForm
     template_name = 'authentication/register.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        mixin_context = self.get_user_context(title='Registration')
-
-        return dict(list(context.items()) + list(mixin_context.items()))
 
     def form_valid(self, form):
         user = form.save()
@@ -25,15 +18,9 @@ class RegisterUserView(DataMixin, CreateView):
         return redirect('home')
 
 
-class LoginUserView(DataMixin, LoginView):
+class LoginUserView(LoginView):
     form_class = LoginUserForm
     template_name = 'authentication/login.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        mixin_context = self.get_user_context(title='Login')
-
-        return dict(list(context.items()) + list(mixin_context.items()))
 
     def get_success_url(self):
         return reverse_lazy('home')
