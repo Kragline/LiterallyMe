8b7ad75f718ec9b36a7949d5712ce5f8f3e83b74
Kragline <armanaraqelyan1107@gmail.com>
2023-08-21 07:26:33 -0700

    django debug toolbar added

--- literallyme/literallyme/settings.py
+++ literallyme/literallyme/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp.apps.MainappConfig",
+    "debug_toolbar",
     "authentication.apps.AuthenticationConfig"
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "literallyme.urls"
@@ -128,3 +130,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- literallyme/literallyme/urls.py
+++ literallyme/literallyme/urls.py
@@ -13,6 +13,11 @@ urlpatterns = [
 
 # only for local server
 if settings.DEBUG:
+    # for django debug toolbar
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
