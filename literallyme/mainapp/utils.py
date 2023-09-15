ad6dca376ed81015f546573d7569ba71b7a28680
Kragline <armanaraqelyan1107@gmail.com>
2023-09-15 09:26:26 -0700

    some final changes in html, css, and users profile

--- literallyme/mainapp/utils.py
+++ literallyme/mainapp/utils.py
@@ -2,16 +2,8 @@ from django.db.models import Count
 from .models import *
 
 
-menu = [
-    {'title': 'Actors', 'url_name': 'actors'},
-    {'title': 'Add actor', 'url_name': 'add_actor'},
-    {'title': 'Add category', 'url_name': 'add_category'},
-    {'title': 'Add movie', 'url_name': 'add_movie'}
-]
-
-
 class DataMixin:
-    paginate_by = 6
+    paginate_by = 9
 
     @classmethod
     def get_categories(cls):
@@ -20,12 +12,7 @@ class DataMixin:
     def get_user_context(self, **kwargs):
         categories = self.get_categories()
 
-        user_menu = menu.copy()
-        if not self.request.user.is_authenticated or not self.request.user.is_superuser:
-            user_menu = [user_menu[0]]
-
         context = kwargs
-        context['menu'] = user_menu
         context['categories'] = categories
 
         return context
